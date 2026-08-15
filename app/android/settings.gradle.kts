4029e95b140f8868379565c6f9efd29412b0d150
lRedRuml <bikoev1996@ya.ru>
2026-08-15 20:56:37 +0300

    Update settings.gradle.kts

--- app/android/settings.gradle.kts
+++ app/android/settings.gradle.kts
@@ -21,6 +21,8 @@ pluginManagement {
 }
 
 plugins {
+    id("com.android.application") version "7.4.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.8.22" apply false
     id("dev.flutter.flutter-gradle-plugin") version "1.0.0" apply false
 }
 
