92d77b454fe3ce2731e1c65cc7de0c36081afcb3
lRedRuml <bikoev1996@ya.ru>
2026-08-15 18:45:16 +0300

    Update settings.gradle.kts

--- app/android/settings.gradle.kts
+++ app/android/settings.gradle.kts
@@ -1,4 +1,15 @@
 pluginManagement {
+    val flutterSdkPath = run {
+        val properties = java.util.Properties()
+        val propertiesFile = settingsDir.resolve("local.properties")
+        if (propertiesFile.exists()) {
+            propertiesFile.inputStream().use { properties.load(it) }
+        }
+        properties.getProperty("flutter.sdk") ?: System.getenv("FLUTTER_ROOT")
+    }
+
+    includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
+
     repositories {
         google()
         mavenCentral()
@@ -7,7 +18,7 @@ pluginManagement {
 }
 
 plugins {
-    id("dev.flutter.flutter-gradle-plugin") version "1.0.0" apply false
+    id("dev.flutter.flutter-gradle-plugin") apply false
     id("com.android.application") version "8.3.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
 }
